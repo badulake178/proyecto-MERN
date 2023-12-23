51384c1ac44208c31ee5537aa6460fb397a0c996
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2023-12-23 14:52:52 -0300

    Coneccion a mongodb atlas listo

---
+++ backend/src/database.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose')
+
+//cadena de conexión
+
+const URI = process.env.MONGODB_URI
+            ? process.env.MONGODB_URI
+            : 'mongodb://localhost/dbtest'
+
+mongoose.connect(URI)
+
+const connection = mongoose.connection;
+
+connection.once('open', ()=>{
+    console.log('La base de datos ha sido conectada: ', URI);
+})
